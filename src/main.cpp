119b6a049ac83367b4be3441b26a633801e03088
Y0L042 <vlamfletcher@gmail.com>
2024-02-25 17:52:56 +0200

    Add libraries

--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,30 @@
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
 #include <iostream>
+#include "imgui.h"
+#include "rlImGui.h"
 
 
 int main(void)
 {
-    //rlImGuiSetup(true);
     InitWindow(800, 450, "raylib [core] example - basic window");
+    rlImGuiSetup(true);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
-            ClearBackground(RAYWHITE);
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-            if ( GuiButton( (Rectangle){ 20, 20, 20, 20 }, "Press me!" ) )
-            {
-                std::cout << "Pressed";
-            }
+        rlImGuiBegin();
+        ClearBackground(RAYWHITE);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        if ( GuiButton( (Rectangle){ 20, 20, 20, 20 }, "Press me!" ) )
+        {
+            std::cout << "Pressed";
+        }
+        ImGui::Text("Hello, world %d", 123);
+        rlImGuiEnd();
         EndDrawing();
     }
-
+    rlImGuiShutdown();
     CloseWindow();
 
     return 0;
